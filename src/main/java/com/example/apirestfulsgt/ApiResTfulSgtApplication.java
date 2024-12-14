f6ba9575523c92d56a4b8666d200717bbfd7a29a
Eduardo Santos <Eduardo_Dsf1@outlook.com>
2024-12-14 15:17:29 -0300

    API

---
+++ src/main/java/com/example/apirestfulsgt/ApiResTfulSgtApplication.java
@@ -0,0 +1,13 @@
+package com.example.apirestfulsgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulSgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiResTfulSgtApplication.class, args);
+    }
+
+}
